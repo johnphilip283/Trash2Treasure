cb8ed9a33082f0d9df6609b9c67f233b8d5bcaaf
nolachen <nolachen@gmail.com>
2018-11-02 01:54:33 -0400

    Volunteer screen done

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
-import { createDrawerNavigator } from 'react-navigation'
+import { createDrawerNavigator } from "react-navigation";
 
-import HomeScreen from './hamburger/HomeScreen';
+import HomeScreen from "./hamburger/HomeScreen";
+import VolunteerScreen from "./hamburger/VolunteerScreen";
 
 export default class App extends Component {
-
-    render() {
-        return (
-            <MyApp />
-        )
-    }
+  render() {
+    return <MyApp />;
+  }
 }
 
 const MyApp = createDrawerNavigator({
-        "Home Screen": HomeScreen,
-        "Settings": HomeScreen,
-        "Volunteer": HomeScreen,
-        "View Tables": HomeScreen,
-        "View Map": HomeScreen,
-        "Search Tags": HomeScreen,
-        "Profile": HomeScreen,
-        "Upload": HomeScreen
-    });
-
+  "Home Screen": HomeScreen,
+  Settings: HomeScreen,
+  Volunteer: VolunteerScreen,
+  "View Tables": HomeScreen,
+  "View Map": HomeScreen,
+  "Search Tags": HomeScreen,
+  Profile: HomeScreen,
+  Upload: HomeScreen
+});
---
+++ components/HamburgerContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+//library imports
+import { Icon, Container, Header, Left, Body, Title, Right } from "native-base";
+
+export default class HamburgerContainer extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Icon name="ios-menu" onPress={this.props.navigation.openDrawer} />
+          </Left>
+          <Body>
+            <Title>{this.props.title}</Title>
+          </Body>
+          <Right />
+        </Header>
+        {this.props.children}
+      </Container>
+    );
+  }
+}
